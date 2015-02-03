86cd31cee5ed58cd5db8ca70d01f95970d0e581b
ishidad <ishidad5@gmail.com>
2015-02-03 18:28:37 -0300

    LoginDto

--- src/main/java/com/globant/carrito/security/LoginDto.java
+++ src/main/java/com/globant/carrito/security/LoginDto.java
@@ -1,12 +1,32 @@
 package com.globant.carrito.security;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import com.globant.carrito.user.User;
+
+import sample.Carrito;
+
 public class LoginDto {
-	private String username;
-	private String password;
-	public String getUsername() {
-		return username;
+	private String userMail;
+	private String userPassword;
+	protected EntityManagerFactory emf;
+
+	  public LoginDto(EntityManagerFactory emf) {
+		  this.emf = emf;
+	  }
+	
+	public String getUserMail(String mail) {		
+		EntityManager em = emf.createEntityManager();
+		userMail = em.createQuery("from User where userMail = "+mail, User.class).getSingleResult();
+		em.close();
+		return userMail;
 	}
-	public String getPassword() {
-		return password;
+	public String getPassword(String pass) {
+		EntityManager em = emf.createEntityManager();
+		userMail = em.createQuery("from User where userPassword = "+pass, User.class).getSingleResult();
+		em.close();
+		return userPassword;
 	}
 }
--- src/main/java/com/globant/carrito/security/SecurityService.java
+++ src/main/java/com/globant/carrito/security/SecurityService.java
@@ -1,6 +1,5 @@
 package com.globant.carrito.security;
 
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -29,5 +28,3 @@ public class SecurityService {
         session.invalidate();
     }
 }
-
-
