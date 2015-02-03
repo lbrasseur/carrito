8319c7a02aae81a69d5d15f589016a6fd4da6653
ishidad <ishidad5@gmail.com>
2015-02-03 16:19:48 -0300

    cambios

--- src/main/java/com/globant/jpa/UserService.java
+++ src/main/java/com/globant/jpa/UserService.java
@@ -5,29 +5,33 @@ import javax.persistence.EntityManagerFactory;
 
 public class UserService {
 	
-	  protected EntityManager em;
+	  protected EntityManagerFactory emf;
 
 	  public UserService(EntityManagerFactory emf) {
-		  EntityManager em = emf.createEntityManager();
-		  this.em = em;
+		  this.emf = emf;
 	  }
 	
 	public User createUser(String mail, String pass, String name){
+		EntityManager em = emf.createEntityManager(); 
 		User emu = new User();
 		emu.setUserMail(mail);
 		emu.setUserPassword(pass);
 		emu.setUserName(name);
+		em.close();
 		return emu;
 	}
 	
 	public void removeUser(String mail) {
-	  User emu = findUser(mail);
-	  if (emu != null) {
-	    em.remove(emu);
+		EntityManager em = emf.createEntityManager(); 
+		User emu = findUser(mail);
+		if (emu != null) {
+			em.remove(emu);
 	  }
+		em.close();
 	}
 
 	public User findUser(String mail) {
+	  EntityManager em = emf.createEntityManager(); 
 	  User u = em.find(User.class, mail);
 	  em.close();
 	  return u;
